40a34bba8175a0136700eca5f45e0d565c7d4b64
ghernandeztt <ghernandez@totaltech.com.mx>
2019-01-31 01:52:17 -0600

    aun no

---
+++ CursoXamarin/CursoXamarin/CursoXamarin/ForgotPasswordPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CursoXamarin
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ForgotPasswordPage : ContentPage
+	{
+        private Label email_Label;
+        private Entry email_Entry;
+        private Label code_Label;
+        private Entry code_Entry;
+
+        public ForgotPasswordPage ()
+		{
+			InitializeComponent ();
+            email_Label = this.FindByName<Label>("emailLabel");
+            email_Entry = this.FindByName<Entry>("emailEntry");
+            code_Label = this.FindByName<Label>("codeLabel");
+            code_Entry = this.FindByName<Entry>("codeEntry");
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(() => {
+                email_Label.IsVisible = false;
+                email_Entry.IsVisible = false;
+                code_Label.IsVisible = true;
+                code_Entry.IsVisible = true;
+            });
+        }
+    }
+}
---
+++ CursoXamarin/CursoXamarin/CursoXamarin/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CursoXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async private void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ForgotPasswordPage());
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            DisplayAlert("Alerta", "Servicio no disponible", "OK");
+        }
+    }
+
+}
